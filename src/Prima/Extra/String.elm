f9eca925f30a4fca47efbb72f571cc42a0da20ba
ascandone <alescandone@gmail.com>
2021-10-19 10:29:06 +0200

    added docs

--- src/Prima/Extra/String.elm
+++ src/Prima/Extra/String.elm
@@ -7,13 +7,19 @@ module Prima.Extra.String exposing (capitalize, isEmpty)
 -}
 
 
-{-| -}
+{-| Checks whether a string is empty
+-}
 isEmpty : String -> Bool
 isEmpty =
     (==) ""
 
 
-{-| -}
+{-|
+
+        capitalize "hello" -- => "Hello"
+        capitalize "Hello" -- => "Hello"
+
+-}
 capitalize : String -> String
 capitalize str =
     (String.left 1 >> String.toUpper) str ++ String.dropLeft 1 str
