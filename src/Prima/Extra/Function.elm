0a4e47c7744aff4bc6ab69c671e6e05c851bde65
ascandone <alescandone@gmail.com>
2021-10-19 10:36:20 +0200

    added docs

--- src/Prima/Extra/Function.elm
+++ src/Prima/Extra/Function.elm
@@ -7,21 +7,42 @@ module Prima.Extra.Function exposing (curry, uncurry, flip)
 -}
 
 
-{-| test docs
+{-| Given a function with a single argument as pair, returns its curried version
+
+        sumPair : (Int, Int) -> Int -> Int
+        sumPair (x, y) = x + y
+
+        curriedSum : Int -> Int -> Int
+        curriedSum x y = curry sumPair
+
 -}
 curry : (( a, b ) -> c) -> a -> b -> c
 curry f x y =
     f ( x, y )
 
 
-{-| test docs
+{-| Given a function with two (curried) arguments, returns a function whose only argument is a pair
+
+        curriedSum : Int -> Int -> Int
+        curriedSum x y = x + y
+
+        sumPair : (Int, Int) -> Int -> Int
+        sumPair = uncurry curriedSum
+
 -}
 uncurry : (a -> b -> c) -> ( a, b ) -> c
 uncurry f ( x, y ) =
     f x y
 
 
-{-| test docs
+{-| Flips the first two (curried) arguments of a function
+
+        cons : a -> List a -> List a
+        cons = (::)
+
+        flippedCons : List a -> a -> List a
+        flippedCons = flip cons
+
 -}
 flip : (a -> b -> c) -> b -> a -> c
 flip mapper b a =
